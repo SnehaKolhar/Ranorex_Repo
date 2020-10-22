f713973671085b6e07ca9827ae7b4e391fe1f108
SnehaKolhar <sneha.kolhar@siemens.com>
2020-10-22 18:48:53 +0530

    Modified solution

--- polarionextrator/Polarion_Json_Run_Extractor.bat
+++ polarionextrator/Polarion_Json_Run_Extractor.bat
@@ -1,4 +1,5 @@
 @echo on
 set project_path=%CD%
 echo %project_path%
+cd %project_path%
 %project_path%\zulu-8-jre\bin\java.exe -jar %project_path%\polarion-test-runs-extractor.jar drivepilot_std "waiting" %project_path%\settings.properties
